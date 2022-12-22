e17b9021bbc71127a016c03d9457d14e04089043
harrisimon <harrisimon@gmail.com>
2022-12-21 19:58:20 -0500

    changed deploy url in config

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: 'https://bam-pow.herokuapp.com',
+	production: 'https://bam-pow.herokuapp.com/bampow',
 	development: 'http://localhost:8000/bampow',
 }
 
